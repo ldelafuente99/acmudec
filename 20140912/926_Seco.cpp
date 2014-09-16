20a463e972461b9e2fba0c471dc317be111b0f98
Diego Seco <dseco@diegoseco.(none)>
2014-09-16 10:05:21 -0300

    926 accepted

--- 20140912/926_Seco.cpp
+++ 20140912/926_Seco.cpp
@@ -8,19 +8,19 @@ http://uva.onlinejudge.org/external/9/p926.pdf
  *
  * Date: 15-09-2014
  *
- * Description: DP
+ * Description: DP, graph traversal
 */
 
 #include <iostream>
 #include <utility>
+#include <string.h>
 
 using namespace std;
 
 unsigned long long int roadsCount[31][31];
-int roadsDisc[31][31];
+bool roadsDisc[31][31][31][31];
 
 unsigned long long int dp(int sx, int sy, int ex, int ey) {
-	//cout << sx << " " << sy << " " << ex << " " << ey << endl;
 	if(sx == ex && sy == ey) {
 		roadsCount[ex][ey]=1;
 		return 1;
@@ -28,9 +28,9 @@ unsigned long long int dp(int sx, int sy, int ex, int ey) {
 	if(roadsCount[sx][sy]!=-1)
 		return roadsCount[sx][sy];
 	roadsCount[sx][sy]=0;
-	if(sx<ex && roadsDisc[sx][sy]!=-4 && roadsDisc[sx+1][sy]!=-3)
+	if(sx<ex && !roadsDisc[sx][sy][sx+1][sy])
 		roadsCount[sx][sy]+=dp(sx+1,sy,ex,ey);
-	if(sy<ey && roadsDisc[sx][sy]!=-1 && roadsDisc[sx][sy+1]!=-2)
+	if(sy<ey && !roadsDisc[sx][sy][sx][sy+1])
 		roadsCount[sx][sy]+=dp(sx,sy+1,ex,ey);
 	return roadsCount[sx][sy];
 }
@@ -41,12 +41,8 @@ int main() {
 	while(testCases--) {
 		int n,numdisc;		
 		cin >> n;
-		for(int i=0; i<=n; i++) {
-			for(int j=0; j<=n; j++) {
-				roadsCount[i][j] = -1;
-				roadsDisc[i][j] = 0;
-			}
-		}
+		memset(roadsCount, -1, sizeof roadsCount);
+		memset(roadsDisc, false, sizeof roadsDisc);
 		pair<int,int> start,end;
 		cin >> start.first >> start.second >> end.first >> end.second >> numdisc;
 		for(int i=0; i<numdisc; i++) {
@@ -54,13 +50,13 @@ int main() {
 			char dir;
 			cin >> discX >> discY >> dir;
 			if(dir=='N') {
-				roadsDisc[discX][discY]=-1;
+				roadsDisc[discX][discY][discX][discY+1]=true;
 			} else if(dir=='S') {
-				roadsDisc[discX][discY]=-2;
+				roadsDisc[discX][discY-1][discX][discY]=true;
 			} else if(dir=='W') {
-				roadsDisc[discX][discY]=-3;
+				roadsDisc[discX-1][discY][discX][discY]=true;
 			} else if(dir=='E') {
-				roadsDisc[discX][discY]=-4;
+				roadsDisc[discX][discY][discX+1][discY]=true;
 			}
 		}
 
