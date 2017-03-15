9a35c80faa334bbd7da94e67ed8bf56263c44e52
silvercobraa <silver.cobra1n@gmail.com>
2017-03-15 18:22:26 -0300

    Primeros 5 problemas :D

---
+++ BolivarCesar/1820.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, char const *argv[])
+{
+	int n, k;
+	scanf("%d", &n);
+	scanf("%d", &k);
+	if (n <= k) {
+		printf("%d\n", 2);
+	}
+	else {
+		printf("%d\n", (int)ceil(2*(n/(double)k)));
+	}
+	return 0;
+}
---
+++ BolivarCesar/1880.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+p1 = int(input())
+e1 = input().split(' ')
+p2 = int(input())
+e2 = input().split(' ')
+p3 = int(input())
+e3 = input().split(' ')
+
+# para que iterar si podemos intersectar conjuntos :v
+eigenvalues = set(e1) & set(e2) & set(e3)
+print(len(eigenvalues))
