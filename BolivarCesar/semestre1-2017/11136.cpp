c42cd1731a500c883628291f258732e9d3d202aa
silvercobraa <silver.cobra1n@gmail.com>
2017-05-25 18:46:20 -0400

    Había que usar long, no int

--- BolivarCesar/semestre1-2017/11136.cpp
+++ BolivarCesar/semestre1-2017/11136.cpp
@@ -6,45 +6,29 @@ using namespace std;
 
 int main(int argc, char const* argv[])
 {
-	int n;
+	// habia que usar long, no bastaba con int
+	long n;
 	cin >> n;
 	for ( ; n != 0; cin >> n) {
-		multiset<int> ms;
-		int cost = 0;
-		for (int i = 0; i < n; i++) {
-			int k;
+		multiset<long> ms;
+		long cost = 0;
+		for (long i = 0; i < n; i++) {
+			long k;
 			cin >> k;
-			for (int j = 0; j < k; j++) {
-				int x;
+			for (long j = 0; j < k; j++) {
+				long x;
 				cin >> x;
 				ms.insert(x);
-				// for (auto vv : ms) {
-				// 	cout << " " << vv;
-				// }
-				// cout << endl;
 			}
-			int minimum = *(ms.begin());
-			int maximum = *(ms.rbegin());
+			long minimum = *(ms.begin());
+			long maximum = *(ms.rbegin());
 			cost += maximum - minimum;
-			// cout << "cost " << cost << endl;
-			// ms.erase(minimum);
-			ms.erase(ms.begin()); // debo pasarle el iterador y no la clave, de lo contrario me borra todos los elementos con la misma clave
-			// for (auto vv : ms) {
-			// 	cout << " " << vv;
-			// }
-			// cout << endl;
+			// ms.erase(ms.begin()); // debo pasarle el iterador y no la clave, de lo contrario me borra todos los elementos con la misma clave
+			ms.erase(ms.find(minimum));
 			// ms.erase(maximum);
 			// ms.erase(ms.rbegin()); // C++ de mierda no me deja hacer lo mismo que hice arriba porque esta wea es un iterador reverso, asi que debo hacer la estupidez de aca abajo
 			ms.erase(ms.find(maximum));
-			// for (auto vv : ms) {
-			// 	cout << " " << vv;
-			// }
-			// cout << endl;
 		}
-		// for (auto vv : ms) {
-		// 	cout << " " << vv;
-		// }
-		// cout << endl;
 		cout << cost << endl;
 	}
 	return 0;
