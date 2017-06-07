e4bc93c061d410adc1afa6b9f911bd13d646804c
silvercobraa <silver.cobra1n@gmail.com>
2017-06-07 18:34:03 -0400

    Código horrible

---
+++ CesarBolivar/Semestre1-2017/00118.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+// esto pude haberlo echo con un arreglo circular, o con puros ifs, pero
+// quería ver si podía inicializar un map de esta manera
+map<char, map<char, char>> m = {
+	{'N', {{'L', 'W'}, {'R', 'E'}}},
+	{'S', {{'L', 'E'}, {'R', 'W'}}},
+	{'E', {{'L', 'N'}, {'R', 'S'}}},
+	{'W', {{'L', 'S'}, {'R', 'N'}}}
+};
+
+int main(int argc, char const* argv[])
+{
+	int xn, yn;
+	cin >> xn >> yn;
+	int x, y;
+	bool grid[xn + 1][yn + 1];
+	char dir;
+	while (cin >> x >> y >> dir) {
+		bool lost = false;
+		// cout << x << ' ' << y << ' ' << dir << endl;
+		string s;
+		cin >> s;
+		// cout << s << endl;
+		// muchos ifs aca abajo
+		for (auto c: s) {
+			if (c != 'F') { dir = m[dir][c]; }
+			else {
+				if (dir == 'N') {
+					if (y == yn) {
+						if (!grid[x][y]) { grid[x][y] = true; lost = true; }
+						else { /* se ignora la instrucción*/ }
+					}
+					else { y++; }
+				}
+				if (dir == 'S') {
+					if (y == 0) {
+						if (!grid[x][y]) { grid[x][y] = true; lost = true; }
+						else { /* se ignora la instrucción*/ }
+					}
+					else { y--; }
+				}
+				if (dir == 'E') {
+					if (x == xn) {
+						if (!grid[x][y]) { grid[x][y] = true; lost = true; }
+						else { /* se ignora la instrucción*/ }
+					}
+					else { x++; }
+				}
+				if (dir == 'W') {
+					if (x == 0) {
+						if (!grid[x][y]) { grid[x][y] = true; lost = true; }
+						else { /* se ignora la instrucción*/ }
+					}
+					else { x--; }
+				}
+			}
+			if (lost) { break; }
+		}
+		cout << x << ' ' << y << ' ' << dir;
+		if (lost) {
+			cout << " LOST";
+		}
+		cout << endl;
+	}
+	return 0;
+}
