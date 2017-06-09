2fbddf18be0e1726fad0f0194d2f437d9f308c01
silvercobraa <silver.cobra1n@gmail.com>
2017-06-09 16:52:12 -0400

    INPUT DE MIERDA

--- CesarBolivar/Semestre1-2017/00168.cpp
+++ CesarBolivar/Semestre1-2017/00168.cpp
@@ -9,35 +9,49 @@ using namespace std;
 
 int main(int argc, char const* argv[])
 {
-	// string s;
-	// for (cin >> s; s != "#"; cin >> s) {
-		// s.back() = ';';
-		map<char, vector<char>> g = {
-			{'A', {'B', 'C', 'D'}},
-			{'B', {'A', 'D'}},
-			{'D', {'B', 'G'}},
-			{'F', {'H'}},
-			{'G', {'D', 'E', 'H'}},
-			{'E', {'F', 'G', 'H'}},
-			{'H', {'E', 'G'}},
-			{'C', {'A', 'D'}},
-		};
+	string s;
+	for (cin >> s; s != "#"; cin >> s) {
+		s.back() = ';';
+		// para que esta wea funcione debo construir un map como el de abajo a partir del input de mierda del problema
+		// map<char, vector<char>> g = {
+		// 	{'A', {'B', 'C', 'D'}},
+		// 	{'B', {'A', 'D'}},
+		// 	{'D', {'B', 'G'}},
+		// 	{'F', {'H'}},
+		// 	{'G', {'D', 'E', 'H'}},
+		// 	{'E', {'F', 'G', 'H'}},
+		// 	{'H', {'E', 'G'}},
+		// 	{'C', {'A', 'D'}},
+		// };
+		map<char, vector<char>> g;
+		for (int i = 0; i < s.size(); i++) {
+			char key = s[i];
+			i++;
+			if (s[i] == ';') {
+				continue;
+			}
+			i++;
+			for ( ; i < s.size() && s[i] != ';'; i++) {
+				g[key].push_back(s[i]);
+			}
+		}
 		// cout << s << endl;
 		char M, T;
 		int k = 0;
 		cin >> M >> T >> k;
-		for (auto p: g) {
-			cout << p.first << ':';
-			for (auto q: p.second) {
-				cout << q;
-			}
-			cout << ';';
-		}
+		// M = 'A'; T = 'C'; k = 3;
+		// for (auto p: g) {
+		// 	cout << p.first << ':';
+		// 	for (auto q: p.second) {
+		// 		cout << q;
+		// 	}
+		// 	cout << ';';
+		// }
 		map<char, bool> lit;
 		vector<char> result;
 		int i = k;
 		while (true) {
-			cout << M << endl;
+			// cout << M << endl;
 			if (i == 0) {
 				if (!lit[T]) {
 					result.push_back(T);
@@ -64,6 +78,6 @@ int main(int argc, char const* argv[])
 			cout << r << ' ';
 		}
 		cout << '/' << M << endl;
-	// }
+	}
 	return 0;
 }
