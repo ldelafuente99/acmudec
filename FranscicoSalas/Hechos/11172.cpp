7d6456f837b111cd62e6d43b5ab14569b1695456
Francisco Salas <frsalas@udec.cl>
2017-08-31 14:04:22 -0300

    asd

---
+++ FranscicoSalas/Hechos/11172.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int cases;
+	int x,y;
+	cin >> cases;
+	for(int i = 0; i < cases; i++){
+		cin >> x >> y;
+		if(x > y)
+			cout << ">" << endl;
+		else if(x < y)
+			cout << "<" << endl;
+		else
+			cout << "=" << endl;
+	}
+
+
+}
